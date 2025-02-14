aa88942be45c8a4bae758317dcd6e75840ad64c8
A01712317 <A01712317@tec.mx>
2025-02-13 22:24:37 -0600

    Agregué estilos, organicé las preguntas en una tabla, añadí funciones js

---
+++ funciones de java/cosa.js
@@ -0,0 +1,18 @@
+console.log("Esto se ve solo en la consola")
+console.info("Esto tambien")
+
+console.warn("WARNING Esto igual pero en amarillo")
+
+console.error("ERROR Esto es un error")
+
+console.log("Este es un ejemplo de template string", 10, "un numero")
+console.assert(1==1);
+console.assert(1===1);
+console.assert(1=="1");
+console.assert(1==="1");
+console.assert(1== true);
+console.assert(1=== true);
+
+//Formas de definir variables
+var cosa="Variable antigua"; //no es segura, se puede modificar desde el navegador
+let cosa1="Variable actual"; //No es modificable 
