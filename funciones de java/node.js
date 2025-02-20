d43cf1028b7e98650bbb226d54e90f3d19fc08ac
A01712317 <A01712317@tec.mx>
2025-02-20 13:34:05 -0600

    ya sirve y entinedo como crear el servidor

--- funciones de java/node.js
+++ funciones de java/node.js
@@ -1,53 +1,3 @@
-console.log("Esto se ve solo en la consola")
-console.info("Esto tambien")
-
-console.warn("WARNING Esto igual pero en amarillo")
-
-console.error("ERROR Esto es un error")
-
-console.log("Este es un ejemplo de template string", 10, "un numero")
-console.assert(1==1);
-console.assert(1===1);
-console.assert(1=="1");
-console.assert(1==="1");
-console.assert(1== true);
-console.assert(1=== true);
-
-//Formas de definir variables
-var cosa="Variable antigua"; //no es segura, se puede modificar desde el navegador
-let cosa1="Variable actual"; //No es modificable 
-alert ("ALERTA ALERTA")
-
-
-
-// script.js
-
-
-//funcion moderna o anónima
-
-const rarisimo = () => {
-    console.log("Esto es una coa rarísima")
-}
-
-
-const boton = document.getElementById("boton") 
-console.log(boton)
-boton.onclick = () =>{
-    console.log("Hiciste click")
-}
-
-const accion = () =>{
-    const imagen = document.getElementById("funspan")
-    imagen.innerHTML='<img alt="Foto de nua planta de menta"  src="https://www.genome.gov/sites/default/files/media/images/tg_es/Gen_es_0.jpg" width="300px" height="200px" >'
-    boton.onclick = otraccion
-
-}
-const otraccion = () =>{
-    const imagen = document.getElementById("funspan")
-    imagen.innerHTML='<img alt="Foto de algo"  src="https://1.bp.blogspot.com/-Yc214sRBGpQ/YHPe_lmXfOI/AAAAAAAAA3c/WGv7jXfKBDMkVV2l0pD4mS0_967EMA5KgCLcBGAsYHQ/s685/Gen.png" width="300px" height="200px" >'
-}
-boton.onclick = accion
-
 //Node
 //Usando node podemos manipular archivos, esto no se puede desde el entorno de un navegador ya que estos protegen los equipos 
 //
@@ -91,7 +41,7 @@ const http = require('http');
 const server = http.createServer( (request, response) => {    
     console.log(request.url);
     response.setHeader('Content-Type', 'text/html');
-    response.write("");
+    response.write("holaaa");
     response.end();
 });
 
