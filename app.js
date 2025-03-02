9444504805db31f6a7ca4fb1849be294f96d6d32
A01712317 <A01712317@tec.mx>
2025-03-02 12:56:03 -0600

    ya funciona con express

--- app.js
+++ app.js
@@ -1,87 +1,27 @@
+const express = require('express');
+const app = express();
 
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const bodyParser = require('body-parser');
 
-// Lee el contenido de index.html
-const indexHTML = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
+app.use(bodyParser.urlencoded({extended: false}));
 
-const server = http.createServer((request, response) => {  
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
 
-    if(request.method == "GET" && (request.url == "/agregar" || request.url == "/")) {
-        console.log(request.url);
-        response.setHeader('Content-Type', 'text/html');
-        response.write(indexHTML);
-        response.end();
-    } else if(request.method == "POST" && request.url == "/agregar") {
-        
-        const datos_completos = [];
-
-        request.on('data', (data)=>{
-            console.log(data);
-            datos_completos.push(data);
-        });
-
-        request.on('end', () => {
-            const string_datos_completos = Buffer.concat(datos_completos).toString();
-            console.log(string_datos_completos);
-
-            const nueva_planta = string_datos_completos.split('=')[1];
-
-            plantas.push(nueva_planta);
-
-            response.setHeader('Content-Type', 'text/html');
-            response.write(indexHTML);
-            
-            response.write(`<div class="columns">`);
-            for(const planta of plantas) {
-                response.write(`<div class="column">`);
-                response.write(`<div class="card">
-                <div class="card-content">
-                    <div class="content">`);
-                response.write(planta);
-                response.write(`</div>
-                    </div>
-                </div>`);
-                response.write(`</div>`);
-            }
-
-            response.end();
-
-        });
-
-    } else if (request.method == "GET" && request.url == "/styles.css") {
-        // Sirve el archivo de estilos CSS
-        fs.readFile(path.join(__dirname, 'styles.css'), 'utf8', (err, data) => {
-            if (err) {
-                response.statusCode = 500;
-                response.end('Error loading styles.css');
-                return;
-            }
-            response.setHeader('Content-Type', 'text/css');
-            response.end(data);
-        });
-    } else if (request.method == "GET" && request.url == "/functions.js") {
-        // Sirve el archivo de funciones JavaScript
-        fs.readFile(path.join(__dirname, 'functions.js'), 'utf8', (err, data) => {
-            if (err) {
-                response.statusCode = 500;
-                response.end('Error loading functions.js');
-                return;
-            }
-            response.setHeader('Content-Type', 'application/javascript');
-            response.end(data);
-        });
-    } else {
-        response.statusCode = 404;
-        response.setHeader('Content-Type', 'text/html');
-        response.write(indexHTML);
-        response.write('<div class="notification is-danger">La página no existe</div>');
-        response.end();
-    }
-
+    //Le permite a la petición avanzar hacia el siguiente middleware
+    next(); 
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+const personajesRoutes = require('./routs/personajes.routs');
+
+app.use('/personajes', personajesRoutes);
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    
+    //Manda la respuesta
+    response.send('¡Hola mundo!'); 
 });
+
+app.listen(3000);
