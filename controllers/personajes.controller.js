c241a3fd47a24f5a55f7ec2e5bbd8c55c8563602
A01712317 <A01712317@tec.mx>
2025-03-06 21:19:14 -0600

    agregué las funciones para interactuar con la base de ddatos pero no funciona jajaaaaaaaa

--- controllers/personajes.controller.js
+++ controllers/personajes.controller.js
@@ -1,12 +1,10 @@
-const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const controller =express.Router();
-controller.use(express.static(path.join(__dirname,'../public')));
+
 
 const Personaje = require('../models/personaje.model');
 
-controller._get_contenido=(request, response, next) => {
+exports._get_contenido=(request, response, next) => {
     console.log(request.session.username);
     response.render('agregar_personaje', {
         isLoggedIn: request.session.isLoggedIn || false,
@@ -14,35 +12,63 @@ controller._get_contenido=(request, response, next) => {
     });
 };
 
-controller._post_peronajes=(request, response, next) => {
-    console.log(request.body);
-    const mi_personaje= new Personaje(request.body.nombre);
-    mi_personaje.save();
-    response.setHeader('Set-Cookie', `ultima_planta=${mi_personaje.nombre}`);
-    response.redirect('/personajes/agregar');
-}
+exports._post_peronajes = (request, response, next) => {
+    const nombre = request.body.nombre;
 
-controller.get_root = (request, response, next) => {
-    console.log(request.get('Cookie'));
-    response.render('_lista_personajes', {
-        isLoggedIn: request.session.isLoggedIn || false,
-        username: request.session.username || '',
-        personajes: Personaje.fetchAll(),
-    });
+    // Validar que el nombre no esté vacío
+    if (!nombre || nombre.trim() === '') {
+        request.session.info = 'El nombre del personaje no puede estar vacío';
+        return response.redirect('/personajes/agregar');
+    }
+
+    const mi_personaje = new Personaje(nombre);
+    mi_personaje.save()
+        .then(() => {
+            request.session.info = `El personaje "${mi_personaje.nombre}" se ha creado`;
+            console.log('Personaje guardado correctamente');
+            response.redirect('/personajes/');
+        })
+        .catch((error) => {
+            console.log('Error al guardar el personaje:', error);
+            request.session.info = 'Error al guardar el personaje';
+            response.redirect('/personajes/agregar');
+        });
 };
 
-controller._get_styles=(request, response, next) => {
+exports.get_root = (request, response, next) => {
+    const mensaje = request.session.info || '';
+    if (request.session.info){
+        request.session.info='';
+    }
+    Personaje.fetch(request.params.id)
+        .then(([rows,fieldData])=>{
+            console.log(fieldData);
+            console.log(rows);
+            response.render('_lista_personajes', {
+                isLoggedIn: request.session.isLoggedIn || false,
+                username: request.session.username || '',
+                personaje: rows,
+                info: mensaje,
+            });
+        }).catch(()=>{
+            console.log('error jaja');
+        }); 
+};
+
+exports.get_body = (request, response, next) => {
+    const path = require('path');
+    response.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+};
+
+exports._get_styles=(request, response, next) => {
     response.sendFile(path.join(__dirname,'../public/css/styles.css'));
 }
 
-controller._get_funciones=(request,response,next) => {
+exports._get_funciones=(request,response,next) => {
     response.sendFile(path.join(__dirname, '../public/funciones.js'));
 }
 
-controller._get_navecacion=(request,response,next) => {
+exports._get_navecacion=(request,response,next) => {
     response.sendFile(path.join(__dirname, '../public/navegacion.js'));
 }
 
-
-
-module.exports = controller
--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,36 +1,33 @@
-const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const controller =express.Router();
 
-controller.get_login=(request, response, next) => {
+exports.get_login=(request, response, next) => {
     response.render('login.ejs',{
         isLoggedIn: request.session.isLoggedIn || false,
         username: request.session.username || '',
     });
 };
 
-controller.post_login=(request, response, next) => {
+exports.post_login=(request, response, next) => {
     request.session.isLoggedIn =true;
     request.session.username = request.body.username;
     response.redirect('/personajes/agregar');
 };
 
-controller.get_logout = (request, response, next) => {
+exports.get_logout = (request, response, next) => {
     request.session.destroy(() => {
         //Este código se ejecuta cuando la sesión se elimina.
         response.redirect('/usuarios/login'); 
     });
 };
-controller._get_styles=(request, response, next) => {
+exports._get_styles=(request, response, next) => {
     response.sendFile(path.join(__dirname,'../public/css/styles.css'));
 }
 
-controller._get_funciones=(request,response,next) => {
+exports._get_funciones=(request,response,next) => {
     response.sendFile(path.join(__dirname, '../public/funciones.js'));
 }
 
-controller._get_navecacion=(request,response,next) => {
+exports._get_navecacion=(request,response,next) => {
     response.sendFile(path.join(__dirname, '../public/navegacion.js'));
 }
-module.exports = controller
